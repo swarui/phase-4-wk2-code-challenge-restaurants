19aebec25629c069cc5cb0368cb680b57d95ce38
swarui <stevewaruim@gmail.com>
2024-01-27 23:30:27 +0300

    initial commit

--- client/src/components/PizzaList.js
+++ client/src/components/PizzaList.js
@@ -24,7 +24,12 @@ const PizzaList = () => {
         <h1>Pizza Menu</h1>
       </div>
       {pizzas.map((pizza) => (
-        
+        <div className="pizza-card" key={pizza.id}>
+          <div className="card">
+            <h2>{pizza.name}</h2>
+            <h3>Ingredients</h3>
+            <p className='pizza-para'>{pizza.ingredients}</p>
+          </div>
         </div>
       ))}
     </div>
